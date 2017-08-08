8f03d3855234aead217c933e726d28c5502bc1a9
邓敦鑫 <dengdunxin@dengdunxindeMacBook-Pro.local>
2017-08-08 14:34:56 +0800

    11

---
+++ src/components/login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="main">
+    <div class="content">
+
+    </div>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less">
+.main {
+ 
+  background: url('../../static/img/login.jpg')
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '../components/home.vue'
+import login from '../components/login.vue'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Home',
-      component: Home
+      name: 'login',
+      component: login
     }
   ]
 })
